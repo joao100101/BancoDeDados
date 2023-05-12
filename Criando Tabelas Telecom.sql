aae3e1d2f34fd06075a1136d0fdc6b3c81fb2429
João Victor Mundel <joaovitor20002@gmail.com>
2023-05-12 10:21:19 -0300

    Update Criando Tabelas Telecom.sql

--- Criando Tabelas Telecom.sql
+++ Criando Tabelas Telecom.sql
@@ -1,5 +1,3 @@
-alter table site change SiteID SiteID int auto_increment;
-alter table equipamento change EquipID EquipID int auto_increment;
 create table Site(SiteID int primary key, nome varchar(30), tipo varchar(30), Tipo_Logradouro varchar(30), Logradouro varchar(30), Numero int, Bairro varchar(30));
 create table Equipamento(EquipID int, Name varchar(30), IP  varchar(30), Modelo varchar(50), Fabricante varchar(50), Shelf varchar(30), DataAquisicao date, Site int, primary key(EquipID, Shelf), foreign key(Site) references Site(SiteID));
 create table Placa(PlacaID int, Tipo varchar(30), Slot int, BandWidth int, EquipID int, primary key(PlacaID), foreign key(EquipID) references Equipamento(EquipID));
